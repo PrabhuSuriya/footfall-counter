67c27e5cea6ec8757b87a06bb6c7cd267015d703
Prabhu Suriya <prabhusuriya.chennai@gmail.com>
2021-05-30 23:08:49 +0530

    add fa; add settings icon

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,11 +6,16 @@ function Header() {
       className="has-background-warning-dark p-1
       is-flex is-flex-direction-row is-justify-content-space-between"
     >
-      <img src="logo192.png" className="image is-32x32" />
-      <h2 className="title has-text-centered has-text-primary-light">
+      <div>
+        <img src="logo192.png" className="image is-32x32" />
+      </div>
+
+      <h2 className="is-flex-grow-1 title has-text-centered has-text-primary-light mb-0">
         Footfall Counter
       </h2>
-
+      <div className="is-flex is-align-items-center">
+        <i className="fas fa-cog has-text-white is-size-4"></i>
+      </div>
     </header>
   );
 }
