f5968cdfe45651368f5c79664b4af5a8ad3ac6dd
Prabhu Suriya <prabhusuriya.chennai@gmail.com>
2021-06-10 21:47:18 +0530

    add mouse wheel support

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,8 +1,17 @@
 import "./Counter.scss";
 
 export default function Counter({ increaseCount, decreaseCount }) {
+  const handleMouseWheel = (e) => {
+    if (e.deltaY > 0) {
+      decreaseCount();
+    }
+    else if (e.deltaY < 0) {
+      increaseCount();
+    }
+
+  }
   return (
-    <div className="buttons is-flex is-justify-content-space-around">
+    <div className="buttons is-flex is-justify-content-space-around" onWheel={e => handleMouseWheel(e)} title="You use your mousewheel to increase/decrease counter!">
       <button className="button is-danger" onClick={decreaseCount}>-</button>
       <button className="button is-success" onClick={increaseCount}>+</button>
     </div>
